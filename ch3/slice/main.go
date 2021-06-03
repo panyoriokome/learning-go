980937857d22840050c4c2f387ae113b74d1019b
Yusuke_Watanabe <yuu-watanabe@ois.okasan.co.jp>
2021-06-03 12:31:15 +0900

    sliceにappendとlenを追加

--- ch3/slice/main.go
+++ ch3/slice/main.go
@@ -23,4 +23,38 @@ func main() {
 	fmt.Println(slice_)
 	fmt.Printf("%T\n", slice_)
 
+	fmt.Println("lenやappend等のBuilt in function")
+	//fmt.Println(slice_ == slice_)
+
+	fmt.Println(len(slice_))
+
+	// 1つの要素を追加
+	slice_ = append(slice_, 10)
+	fmt.Println(slice_) // [10]
+
+	// 複数要素を追加
+	slice_ = append(slice_, 20, 30, 40)
+	fmt.Println(slice_) // [10 20 30 40]
+
+	var y = []int{50, 60, 70}
+	slice_ = append(slice_, y...)
+	fmt.Println(slice_) // [10 20 30 40 50 60 70]
+
+	// Goはcall by valueの言語であるため、返り値を変数に代入しないとエラーになる
+	// append(slice_, y...) // append(slice_, y...) evaluated but not used
+
+	fmt.Println("Capacity")
+
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 10)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 20)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 30)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 40)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 50)
+	fmt.Println(x, len(x), cap(x))
 }
