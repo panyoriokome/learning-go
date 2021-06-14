6344746e296adf715a6c73bc38b4c78c44bad4da
Yusuke_Watanabe <yuu-watanabe@ois.okasan.co.jp>
2021-06-14 09:26:38 +0900

    if文を追加

--- ch4/if/main.go
+++ ch4/if/main.go
@@ -6,14 +6,23 @@ import (
 )
 
 func main() {
-	n := rand.Intn(10)
-	fmt.Println(n) // 常に1
+	// n := rand.Intn(10)
+	// fmt.Println(n) // 常に1
 
-	if n == 0 {
-		fmt.Println("低すぎる")
+	// if n == 0 {
+	// 	fmt.Println("低すぎる")
+	// } else if n > 5 {
+	// 	fmt.Println("高すぎる")
+	// } else {
+	// 	fmt.Println("ちょうどいい") // 常に1なのでここが表示される
+	// }
+
+	if n := rand.Intn(10); n == 0 {
+		fmt.Println("低すぎる", n) //
 	} else if n > 5 {
-		fmt.Println("高すぎる")
+		fmt.Println("高すぎる", n)
 	} else {
-		fmt.Println("ちょうどいい") // 常に1なのでここが表示される
+		fmt.Println("ちょうどいい", n)
 	}
+	fmt.Println(n) // undefined: n
 }
